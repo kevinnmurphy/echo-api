e13f63ccfaf7ea6184b9c0bd097d04d0f8d9af37
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-09-27 13:53:27 -0400

    cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,5 @@ class User < ApplicationRecord
     has_many :user_playlists, dependent: :destroy
     has_many :playlists, through: :user_playlists
 
-    # accepts_nested_attributes_for :playlists, reject_if: :all_blank
-
     validates :name, presence: true
-    # validates :email, uniqueness: true
 end
