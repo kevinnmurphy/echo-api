ffca1be5354dbf2f61e68e1701afd3d68bd4f4b3
KevinnMurphy <kevinnealmurphy@gmail.com>
2020-10-23 15:18:02 -0400

    make it better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
-    helper_method :current_user
+    # helper_method :current_user
 
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-    end
+    # def current_user
+    #     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    # end
 end
